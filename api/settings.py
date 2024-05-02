ab643c8a77dea52dac3ad55fd5dc2197d21b6e8d
BMWdejf <sablatura.david@gmail.com>
2024-05-02 17:00:19 +0200

    Register new Django app "Products" to settings.py

--- api/settings.py
+++ api/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example'
+    'example.apps.ExampleConfig',
+    'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
